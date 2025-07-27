bd2620a9b96732b6c16a0d7ee0d16c9169abc9e0
Neeraj <neeraj.y@oliv.ai>
2025-07-27 17:24:53 +0530

    debug statements

--- services/emailProcessor.service.js
+++ services/emailProcessor.service.js
@@ -8,6 +8,7 @@ const { appendExpenses, getCategories } = require('./googleSheets.service');
 const pdfPasswords = require('../config/pdfPasswords');
 
 function getPasswordForFile(fileName) {
+  console.log('Searching for password for file:', fileName, pdfPasswords);
   const name = fileName.toLowerCase();
   for (const config of pdfPasswords) {
     for (const keyword of config.keywords) {
